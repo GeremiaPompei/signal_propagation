fix mixed precision and surrogate

main.py
@@ -11,11 +11,11 @@ from src.utils.select_device import select_device
 
 
 def main():
-    set_seed(0)
     device = select_device()
 
+    set_seed(0)
     TR_SET, TS_SET = mnist_loader(device=device)
-    model = ConvSpikeNN(num_classes=10, activation_constructor=SurrogateActivation)
+    model = ConvSpikeNN(num_classes=10, activation_constructor=lambda: SurrogateActivation(spike_threshold=0.5))
     trainer = SigpropTrainer(model, device=device, precision=torch.bfloat16)
 
     trainer(TR_SET, TS_SET)

src/model/spike_conv_nn.py
@@ -9,7 +9,7 @@ class SpikeActivation(torch.nn.Module):
         self.spike_threshold = spike_threshold
         self.reset_value = reset_value
 
-    def __call__(self, inputs):
+    def forward(self, inputs):
         inputs = inputs.split(inputs.shape[0] // self.times)
         outputs = []
         V = torch.zeros_like(inputs[0])
@@ -22,8 +22,15 @@ class SpikeActivation(torch.nn.Module):
 
 class SurrogateActivation(torch.nn.Module):
 
-    def __call__(self, inputs):
-        return 1 / torch.pi * torch.arctan(torch.pi * inputs) + 1 / 2
+    def __init__(self, spike_threshold=1, reset_value=0):
+        super().__init__()
+        self.spike_threshold = spike_threshold
+        self.reset_value = reset_value
+
+    def forward(self, inputs):
+        x = 1 / torch.pi * torch.arctan(torch.pi * inputs) + 1 / 2
+        x[x.abs() > self.spike_threshold] = self.reset_value
+        return x
 
 
 class ConvBlock(torch.nn.Module):

src/trainer/sigprop.py
@@ -1,3 +1,5 @@
+from typing import Callable
+
 import torch
 
 from src.trainer.trainer import Trainer
@@ -14,9 +16,19 @@ def get_leaf_layers(module: torch.nn.Module, device: str = 'cpu'):
 
 
 class SigpropTrainer(Trainer):
-    def __init__(self, model: torch.nn.Module, device: str = 'cpu', precision: torch.dtype = torch.float32):
+    def __init__(
+            self,
+            model: torch.nn.Module,
+            device: str = 'cpu',
+            precision: torch.dtype = torch.float32,
+            inner_layer_distance_function: Callable = None
+    ):
         super().__init__(model, device, precision)
         self.initialized = False
+        if inner_layer_distance_function is None:
+            self.inner_layer_distance_function = lambda h_n, t_n: (h_n - t_n).pow(2).mean()
+        else:
+            self.inner_layer_distance_function = inner_layer_distance_function
 
     def __initialize__(self, TR_X_MB, TR_Y_MB):
         if not self.initialized:
@@ -36,20 +48,21 @@ class SigpropTrainer(Trainer):
             h, t = TR_X_MB, TR_Y_MB
         for i, layer in enumerate(self.layers):
             h.requires_grad, t.requires_grad = True, True
-            if i > 0:
-                if type(layer) == torch.nn.Linear:
-                    h = h.view(h.shape[0], -1)
-                    t = t.view(t.shape[0], -1)
-                h_n = layer(h)
-                t_n = layer(t)
-            else:
-                h_n = layer(h)
-                t_n = self.output_embedding_layer(t).view(-1, self.dim_c, self.dim_w, self.dim_h)
+            with torch.autocast(device_type=self.device, dtype=self.precision):
+                if i > 0:
+                    if type(layer) == torch.nn.Linear:
+                        h = h.view(h.shape[0], -1)
+                        t = t.view(t.shape[0], -1)
+                    h_n = layer(h)
+                    t_n = layer(t)
+                else:
+                    h_n = layer(h)
+                    t_n = self.output_embedding_layer(t).view(-1, self.dim_c, self.dim_w, self.dim_h)
+                if i == len(self.layers) - 1:
+                    loss = torch.nn.functional.cross_entropy(h_n, TR_Y_MB)
+                else:
+                    loss = self.inner_layer_distance_function(h_n, t_n)
             self.optim.zero_grad()
-            if i == len(self.layers) - 1:
-                loss = torch.nn.functional.cross_entropy(h_n, TR_Y_MB)
-            else:
-                loss = (h_n - t_n).pow(2).mean()
             loss.backward()
             self.optim.step()
             h, t = h_n.detach(), t_n.detach()

src/trainer/trainer.py
@@ -25,23 +25,22 @@ class Trainer(ABC):
             TS_SET,
             epochs=64,
     ):
-        self.model = self.model.to(self.precision)
-
         log.info(self.__class__.__name__)
         for epoch in range(epochs):
             tr_loss_sum, ts_loss_sum = 0, 0
 
             self.model.train()
             for TR_X_MB, TR_Y_MB in TR_SET:
-                tr_loss_sum += self.train_mb(TR_X_MB.to(self.precision), TR_Y_MB.to(self.precision))
+                tr_loss_sum += self.train_mb(TR_X_MB, TR_Y_MB)
             tr_loss = tr_loss_sum / len(TR_SET)
 
             self.model.eval()
             for X_MB, Y_MB in TS_SET:
-                ts_loss_sum += torch.nn.functional.cross_entropy(
-                    self.model(X_MB.to(self.precision)),
-                    Y_MB.to(self.precision)
-                )
+                with torch.autocast(device_type=self.device, dtype=self.precision):
+                    ts_loss_sum += torch.nn.functional.cross_entropy(
+                        self.model(X_MB),
+                        Y_MB
+                    )
             ts_loss = ts_loss_sum / len(TS_SET)
 
             log.info(f'epoch: {epoch + 1:>4}/{epochs} - tr_loss: {tr_loss:>10.6f} - ts_loss: {ts_loss:>10.6f}')