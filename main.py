8d884c38667a06fc156a66d84a65c5b70ba3cb5d
Geremia Pompei <geremiapompei@gmail.com>
2023-08-11 19:23:08 +0200

    optimization

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ def main():
 
     set_seed(0)
     TR_SET, TS_SET = mnist_loader(device=device)
-    model = ConvSpikeNN(num_classes=10, activation_constructor=lambda: SurrogateActivation(spike_threshold=0.5))
+    model = ConvSpikeNN(num_classes=10, activation_constructor=lambda: SurrogateActivation(spike_threshold=1))
     trainer = SigpropTrainer(model, device=device, precision=torch.bfloat16)
 
     trainer(TR_SET, TS_SET)
--- src/loader/pytorch_mnist_dataset_loader.py
+++ src/loader/pytorch_mnist_dataset_loader.py
@@ -1,10 +1,29 @@
 import os
-from typing import Callable, Tuple, Any
+from typing import Callable
 
 import torch
-import torchvision
-from torchvision.transforms import transforms
-from torch.utils.data.dataloader import default_collate
+from torch.utils.data import Dataset
+
+
+class MNISTDataLoader(Dataset):
+
+    def __init__(self, data, batch_size=128, device: str = 'cpu', n_classes: int = 10):
+        X = data.train_data.type(torch.float)
+        perm = torch.randperm(X.shape[0])
+        self.X = X[perm].split(batch_size, 0)
+        self.Y = torch.nn.functional.one_hot(
+            data.train_labels.type(torch.LongTensor)[perm], n_classes)\
+            .type(torch.int8)\
+            .split(batch_size, 0)
+        self.device = device
+
+    def __len__(self):
+        return len(self.X)
+
+    def __getitem__(self, idx):
+        X = self.X[idx].reshape(-1, 1, 28, 28).to(self.device)
+        Y = self.Y[idx].to(torch.float).to(self.device)
+        return X, Y
 
 
 def pytorch_mnist_dataset_loader(
@@ -26,16 +45,15 @@ def pytorch_mnist_dataset_loader(
     """
     if not os.path.exists(dir_name):
         os.mkdir(dir_name)
-    TR_MNIST = dataset_loader_func(root=dir_name, train=True, download=True, transform=None)
-    TS_MNIST = dataset_loader_func(root=dir_name, train=False, download=True, transform=None)
-    TR_SET = \
-        TR_MNIST.train_data.type(torch.float).reshape(-1, 1, 28, 28).to(device), \
-        torch.nn.functional.one_hot(TR_MNIST.train_labels).to(device)
-    TS_SET = \
-        TS_MNIST.test_data.type(torch.float).reshape(-1, 1, 28, 28).to(device), \
-        torch.nn.functional.one_hot(TS_MNIST.test_labels).to(device)
+    TR_MNIST = MNISTDataLoader(
+        dataset_loader_func(root=dir_name, train=True, download=True, transform=None),
+        batch_size=batch_size,
+        device=device,
+    )
+    TS_MNIST = MNISTDataLoader(
+        dataset_loader_func(root=dir_name, train=False, download=True, transform=None),
+        batch_size=batch_size,
+        device=device,
+    )
 
-    TR_X, TR_Y = [x.type(torch.float32).split(batch_size, 0) for x in TR_SET]
-    TS_X, TS_Y = [x.type(torch.float32).split(batch_size, 0) for x in TS_SET]
-
-    return tuple(zip(TR_X, TR_Y)), tuple(zip(TS_X, TS_Y))
+    return TR_MNIST, TS_MNIST
