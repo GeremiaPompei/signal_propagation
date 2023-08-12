fix precision

main.py
@@ -12,11 +12,12 @@ from src.utils.select_device import select_device
 
 def main():
     device = select_device()
+    precision = torch.bfloat16
 
     set_seed(0)
-    TR_SET, TS_SET = mnist_loader(device=device)
+    TR_SET, TS_SET = mnist_loader(device=device, precision=precision)
     model = ConvSpikeNN(num_classes=10, surrogate=False)
-    trainer = SigpropTrainer(model, device=device, precision=torch.bfloat16)
+    trainer = SigpropTrainer(model, device=device, precision=precision)
 
     trainer(TR_SET, TS_SET)
 

src/loader/mnist_loader.py
@@ -1,9 +1,10 @@
+import torch
 from torchvision import datasets
 
 from src.loader.pytorch_mnist_dataset_loader import pytorch_mnist_dataset_loader
 
 
-def mnist_loader(device='cpu'):
+def mnist_loader(device: str = 'cpu', precision: torch.dtype = torch.float32):
     """
     Function able to download MNIST dataset and return it.
     :param device: Accelerator where allocate the dataset.
@@ -16,4 +17,5 @@ def mnist_loader(device='cpu'):
         dir_name='MNIST/',
         dataset_loader_func=datasets.MNIST,
         device=device,
+        precision=precision,
     )

src/trainer/backpropagation.py
@@ -6,9 +6,8 @@ from src.trainer.trainer import Trainer
 class BackpropagationTrainer(Trainer):
     def train_mb(self, TR_X_MB: torch.Tensor, TR_Y_MB: torch.Tensor):
         self.optim.zero_grad()
-        with torch.autocast(device_type=self.device, dtype=self.precision):
-            TR_P_MB = self.model(TR_X_MB)
-            loss = self.criterion(TR_P_MB, TR_Y_MB)
+        TR_P_MB = self.model(TR_X_MB)
+        loss = self.criterion(TR_P_MB, TR_Y_MB)
         loss.backward()
         self.optim.step()
         return loss.item()

src/trainer/sigprop.py
@@ -20,7 +20,7 @@ class SigpropTrainer(Trainer):
             self,
             model: torch.nn.Module,
             device: str = 'cpu',
-            precision: torch.dtype = torch.float32,
+            precision: torch.dtype = None,
             inner_layer_distance_function: Callable = None
     ):
         super().__init__(model, device, precision)
@@ -49,19 +49,18 @@ class SigpropTrainer(Trainer):
         for i, layer in enumerate(self.layers):
             self.optim.zero_grad()
             h.requires_grad, t.requires_grad = True, True
-            with torch.autocast(device_type=self.device, dtype=self.precision):
-                if i > 0:
-                    if type(layer) == torch.nn.Linear:
-                        h = h.view(h.shape[0], -1)
-                        t = t.view(t.shape[0], -1)
-                    h_n, t_n = layer(torch.cat((h, t))).tensor_split(2)
-                else:
-                    h_n = layer(h)
-                    t_n = self.output_embedding_layer(t).view(-1, self.dim_c, self.dim_w, self.dim_h)
-                if i == len(self.layers) - 1:
-                    loss = torch.nn.functional.cross_entropy(h_n, TR_Y_MB)
-                else:
-                    loss = self.inner_layer_distance_function(h_n, t_n)
+            if i > 0:
+                if type(layer) == torch.nn.Linear:
+                    h = h.view(h.shape[0], -1)
+                    t = t.view(t.shape[0], -1)
+                h_n, t_n = layer(torch.cat((h, t))).tensor_split(2)
+            else:
+                h_n = layer(h)
+                t_n = self.output_embedding_layer(t).view(-1, self.dim_c, self.dim_w, self.dim_h)
+            if i == len(self.layers) - 1:
+                loss = torch.nn.functional.cross_entropy(h_n, TR_Y_MB)
+            else:
+                loss = self.inner_layer_distance_function(h_n, t_n)
             try:
                 loss.backward()
                 self.optim.step()

src/trainer/trainer.py
@@ -7,13 +7,13 @@ from abc import ABC, abstractmethod
 
 class Trainer(ABC):
 
-    def __init__(self, model: torch.nn.Module, device: str = 'cpu', precision: torch.dtype = torch.float32):
+    def __init__(self, model: torch.nn.Module, device: str = 'cpu', precision: torch.dtype = None):
         self.device = device
-        self.model = model.to(self.device)
+        self.precision = precision
+        self.model = model.to(self.precision).to(self.device)
         self.criterion = torch.nn.CrossEntropyLoss()
         self.optim = torch.optim.Adam(self.model.parameters(), lr=5e-4)
         self.lrs = torch.optim.lr_scheduler.CosineAnnealingLR(self.optim, T_max=64)
-        self.precision = precision
 
     @abstractmethod
     def train_mb(self, TR_X_MB: torch.Tensor, TR_Y_MB: torch.Tensor):
@@ -37,11 +37,10 @@ class Trainer(ABC):
 
             self.model.eval()
             for X_MB, Y_MB in tqdm(TS_SET, desc=f'{epoch + 1}/{epochs} evaluation'):
-                with torch.autocast(device_type=self.device, dtype=self.precision):
-                    ts_loss_sum += torch.nn.functional.cross_entropy(
-                        self.model(X_MB),
-                        Y_MB
-                    )
+                ts_loss_sum += torch.nn.functional.cross_entropy(
+                    self.model(X_MB),
+                    Y_MB
+                )
             ts_loss = ts_loss_sum / len(TS_SET)
 
             log.info(f'epoch: {epoch + 1:>4}/{epochs} - tr_loss: {tr_loss:>10.6f} - ts_loss: {ts_loss:>10.6f}')