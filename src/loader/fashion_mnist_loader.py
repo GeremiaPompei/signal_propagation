precision fix

src/loader/fashion_mnist_loader.py
@@ -1,10 +1,9 @@
-import torch
 from torchvision import datasets
 
 from src.loader.pytorch_mnist_dataset_loader import pytorch_mnist_dataset_loader
 
 
-def fashion_mnist_loader(device='cpu', precision: torch.dtype = torch.float32):
+def fashion_mnist_loader(device='cpu'):
     """
     Function able to download Fashion-MNIST dataset and return it.
     :param device: Accelerator where allocate the dataset.
@@ -17,5 +16,4 @@ def fashion_mnist_loader(device='cpu', precision: torch.dtype = torch.float32):
         dir_name='Fashion-MNIST/',
         dataset_loader_func=datasets.FashionMNIST,
         device=device,
-        precision=precision,
     )

src/loader/pytorch_mnist_dataset_loader.py
@@ -12,11 +12,9 @@ class MNISTDataLoader(Dataset):
             data,
             batch_size=128,
             device: str = 'cpu',
-            precision: torch.dtype = torch.float32,
             n_classes: int = 10
     ):
         self.device = device
-        self.precision = precision
         X = data.train_data.type(torch.int8)
         perm = torch.randperm(X.shape[0])
         self.X = X[perm].split(batch_size, 0)
@@ -27,10 +25,10 @@ class MNISTDataLoader(Dataset):
         return len(self.X)
 
     def __getitem__(self, idx):
-        X = self.X[idx].to(self.precision).reshape(-1, 1, 28, 28).to(self.device)
+        X = self.X[idx].to(torch.float).reshape(-1, 1, 28, 28).to(self.device)
         Y = torch.nn.functional.one_hot(
             self.Y[idx].type(torch.LongTensor), self.n_classes
-        ).to(self.precision).to(self.device)
+        ).to(torch.float).to(self.device)
         return X, Y
 
 
@@ -39,7 +37,6 @@ def pytorch_mnist_dataset_loader(
         dataset_loader_func: Callable,
         batch_size: int = 128,
         device: str = 'cpu',
-        precision: torch.dtype = torch.float32,
 ) -> tuple:
     """
     Function able to download pytorch MNIST like dataset and return it.
@@ -58,13 +55,11 @@ def pytorch_mnist_dataset_loader(
         dataset_loader_func(root=dir_name, train=True, download=True, transform=None),
         batch_size=batch_size,
         device=device,
-        precision=precision,
     )
     TS_MNIST = MNISTDataLoader(
         dataset_loader_func(root=dir_name, train=False, download=True, transform=None),
         batch_size=batch_size,
         device=device,
-        precision=precision,
     )
 
     return TR_MNIST, TS_MNIST

src/trainer/backpropagation.py
@@ -6,8 +6,9 @@ from src.trainer.trainer import Trainer
 class BackpropagationTrainer(Trainer):
     def train_mb(self, TR_X_MB: torch.Tensor, TR_Y_MB: torch.Tensor):
         self.optim.zero_grad()
-        TR_P_MB = self.model(TR_X_MB)
-        loss = self.criterion(TR_P_MB, TR_Y_MB)
+        with torch.autocast(device_type=self.device, dtype=self.precision):
+            TR_P_MB = self.model(TR_X_MB)
+            loss = self.criterion(TR_P_MB, TR_Y_MB)
         loss.backward()
         self.optim.step()
         return loss.item()